7e44176985538be5487e39b54229a33e0feafad6
ksj <kimsj9693@gmail.com>
2024-05-09 15:27:51 +0900

    Hooks 실습, 이벤트 핸들러 실습

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 // import Library from './chapter_03/Library';
 // import Clock from './chapter_04/clock';
 // import CommentList from './chapter_05/comment-list';
-import NotificationList from './chapter_06/NotificationList';
+// import NotificationList from './chapter_06/NotificationList';
+// import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/confirmbutton';
 
 // React 18에서 새로운 root API 사용
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -34,14 +36,29 @@ import NotificationList from './chapter_06/NotificationList';
 //   document.getElementById('root')
 // );
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <NotificationList />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Accommodate />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <ConfirmButton />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
